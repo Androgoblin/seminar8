abb96e1609c733771af1e8cc33f33be3bdf8bf4d
Radik <radikrah78@gmail.com>
2023-06-18 22:24:57 +0300

    Семинар 4 дополнил

--- Seminar/sem4/Program.cs
+++ Seminar/sem4/Program.cs
@@ -1,4 +1,5 @@
-﻿////Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+﻿////Задача 1. Напишите программу, которая принимает на вход число (А)
+//  и выдаёт сумму чисел от 1 до А.
 
 //7 -> 28
 //4 -> 10
@@ -23,7 +24,7 @@
 
 // [1,0,1,1,0,1,0,0]
 
-// int[]  CreateRandomArray(int size, mineValue, maxeValue)
+// int[] CreateRandomArray(int size,int mineValue,int maxeValue)
 // {
 //   int [] array = new int[size];
 //   for (int i = 0; i < size; i++)
@@ -37,26 +38,27 @@
 // {
 //     for (int i=0 ; i< array.Length; i++)
 //     {
-//         System.Console.WriteLine(array(i) + " ");
+//         System.Console.Write(array[i] + " ");
 //     }
 //     System.Console.WriteLine();
 // }
 //   System.Console.WriteLine("input array size: ");
-//   int a = Convert.ToInt32(Console.ReadLine());
+//   int size = Convert.ToInt32(Console.ReadLine());
 //   System.Console.WriteLine("input minimal value of array element: ");
-//   int a = Convert.ToInt32(Console.ReadLine());
-//   System.Console.WriteLine("input array size: ");
-//   int a = Convert.ToInt32(Console.ReadLine());
+//   int minValue = Convert.ToInt32(Console.ReadLine());
+//   System.Console.WriteLine("input maximal value of array element:");
+//   int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// PrintArray(CreateRandomArray(size, minValue, maxValue));
 
 
+// Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
 
-//Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
 
-//456 -> 3
-//78 -> 2
-//89126 -> 5
 
-// 
 // }int FindNum(int a)
 // {  
 //     int current = 0;
@@ -70,7 +72,8 @@
 // int a = Convert.ToInt32(Console.ReadLine());
 // System.Console.WriteLine($"{a} -> {FindNum(a)}");
 
-//Задача 3. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//Задача 3. Напишите программу, которая принимает на вход число N 
+//  и выдаёт произведение чисел от 1 до N.
 
 //4  -> 24
 //5  -> 120
